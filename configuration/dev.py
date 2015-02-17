8043132c8771e3cfe8eca9ff741bc466bbff8204
Renzo Lucioni <renzo@renzolucioni.com>
2015-02-16 23:23:58 -0500

    Fill in projects

--- configuration/dev.py
+++ configuration/dev.py
@@ -97,25 +97,40 @@ NOT_FOUND_SAVE_AS = '404.html'
 # A list of project metadata, used to generate the projects page
 PROJECTS = [
     {
-        'title': "Awesome Project",
-        'link': 'http://www.reddit.com/',
+        'title': "Personal Website",
+        'link': 'https://github.com/rlucioni/pelican-site',
         'project_id': 'pelican-site',
-        'image_path': 'http://placehold.it/800x400',
-        'description': "This is my latest project. It is awesome.",
+        # Dimensions should be 1000x600
+        'image_filename': 'pelican-site.png',
+        'description': "This is the source for the website you're currently visiting. It's a static site generated with Pelican.",
     },
     {
-        'title': "A Sweet Project",
-        'link': 'http://www.reddit.com/',
-        'project_id': 'edx-platform',
-        'image_path': 'http://placehold.it/800x400',
-        'description': "This is one of my later projects. It's pretty sweet!",
+        'title': "Search Term Cards",
+        'link': 'https://github.com/rlucioni/search-term-cards',
+        'project_id': 'search-term-cards',
+        'image_filename': 'search-term-card.png',
+        'description': "This is a Flask app which displays site-search terms pulled from Google Analytics. It was inspired by and modeled after Google's own \"Hot Searches\" visualization. It's used to display popular search terms at edX's Cambridge office.",
     },
     {
-        'title': "First Project",
-        'link': 'http://www.reddit.com/',
-        'project_id': 'pelican-site',
-        'image_path': 'http://placehold.it/800x400',
-        'description': "This was my first project. It was cool.",
+        'title': "Yo Notifier",
+        'link': 'https://github.com/rlucioni/yo-notifier',
+        'project_id': 'yo-notifier',
+        'image_filename': 'yo-notifier.png',
+        'description': "This is a small Flask app which uses webhooks to send event-based Yo notifications. I use it to send push notifications to my phone.",
+    },
+    {
+        'title': "Real-time Dashboard",
+        'link': 'https://github.com/rlucioni/realtime-dashboard',
+        'project_id': 'realtime-dashboard',
+        'image_filename': 'pulse.gif',
+        'description': "This is a Flask app which uses server-sent events to update a dashboard in real-time. I've used it to visualize registrations and enrollments on edX in real-time.",
+    },
+    {
+        'title': "Project Euler Solutions",
+        'link': 'https://github.com/rlucioni/project-euler',
+        'project_id': 'project-euler',
+        'image_filename': 'euler.png',
+        'description': "This ongoing project includes my solutions to Project Euler problems, as well as a utility for timing the execution of solutions on different inputs.",
     },
 ]
 
