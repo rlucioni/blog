48d00b94f3a39e42536f88991d5594dc7ff4dd20
Renzo Lucioni <renzo@renzolucioni.com>
2016-07-03 23:03:50 -0400

    Simplify base template and remove Segment

--- configuration/dev.py
+++ configuration/dev.py
@@ -5,8 +5,8 @@ from datetime import datetime
 
 
 ##### Basic settings #####
-AUTHOR = "Renzo Lucioni"
-SITENAME = "Renzo Lucioni"
+AUTHOR = 'Renzo Lucioni'
+SITENAME = 'Renzo Lucioni'
 SITE_DESCRIPTION = "Renzo Lucioni's personal website and portfolio."
 SITEURL = ''
 
@@ -93,7 +93,6 @@ PROJECTS = [
     {
         'title': "Personal Website",
         'link': 'https://github.com/rlucioni/pelican-site',
-        'project_id': 'pelican-site',
         # Dimensions should be 1000x600
         'image_filename': 'pelican-site.png',
         'description': "This is the source for the website you're currently visiting. It's a static site generated with Pelican.",
@@ -101,28 +100,24 @@ PROJECTS = [
     {
         'title': "Search Term Cards",
         'link': 'https://github.com/rlucioni/search-term-cards',
-        'project_id': 'search-term-cards',
         'image_filename': 'search-term-card.png',
         'description': "This is a Flask app which displays site-search terms pulled from Google Analytics. It was inspired by and modeled after Google's own \"Hot Searches\" visualization. It's used to display popular search terms at edX's Cambridge office.",
     },
     {
         'title': "Yo Notifier",
         'link': 'https://github.com/rlucioni/yo-notifier',
-        'project_id': 'yo-notifier',
         'image_filename': 'yo-notifier.png',
         'description': "This is a small Flask app which uses webhooks to send event-based Yo notifications. I use it to send push notifications to my phone.",
     },
     {
         'title': "Real-time Dashboard",
         'link': 'https://github.com/rlucioni/realtime-dashboard',
-        'project_id': 'realtime-dashboard',
         'image_filename': 'pulse.gif',
         'description': "This is a Flask app which uses server-sent events to update a dashboard in real-time. I've used it to visualize registrations and enrollments on edX in real-time.",
     },
     {
         'title': "Project Euler Solutions",
         'link': 'https://github.com/rlucioni/project-euler',
-        'project_id': 'project-euler',
         'image_filename': 'euler.png',
         'description': "This ongoing project includes my solutions to Project Euler problems, as well as a utility for timing the execution of solutions on different inputs.",
     },
@@ -132,17 +127,9 @@ PROJECTS_SAVE_AS = 'projects/index.html'
 
 GITHUB_URL = 'https://github.com/rlucioni/'
 
-# Whether InstantClick (https://github.com/dieulot/instantclick) should be used to preload pages
-INSTANT_CLICK = True
-
-# Whether FastClick (https://github.com/ftlabs/fastclick) should be used to remove click
-# delays in browsers with touch UIs
-FAST_CLICK = True
-
 # These are dev keys. When a key is provided, the feature it corresponds to is enabled; to
 # disable a feature, set its key to None.
 GOOGLE_ANALYTICS_KEY = 'UA-33031883-4'
-SEGMENT_KEY = 'KBYipO19jAaiwTxCCVkiD8MRcULFhAEz'
 
 ##### Plugins #####
 PLUGIN_PATHS = ['../utilities']
