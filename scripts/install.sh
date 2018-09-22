1a3db868c4919fd116c688030c0df2bf201ab29f
Renzo Lucioni <renzo@lucioni.xyz>
2018-09-22 16:37:39 -0400

    Revert "no sudo"
    This reverts commit 57879b215f619c022718986622981662f6f823df.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 
 # workaround required to avoid libstdc++ errors while running "extended" hugo with SASS support
 wget http://security.ubuntu.com/ubuntu/pool/main/g/gcc-5/libstdc++6_5.4.0-6ubuntu1~16.04.10_amd64.deb
-dpkg --force-all -i libstdc++6*.deb
+sudo dpkg --force-all -i libstdc++6*.deb
 
 wget -O - https://raw.githubusercontent.com/creationix/nvm/v${NVM_VERSION}/install.sh | bash
 source ~/.nvm/nvm.sh
@@ -16,6 +16,6 @@ npm --version
 npm install
 
 wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.deb"
-dpkg -i hugo*.deb
+sudo dpkg -i hugo*.deb
 
 hugo version
