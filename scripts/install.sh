5471f6ac2233bf951b72190750182bd057e7d24e
Renzo Lucioni <renzo@lucioni.xyz>
2018-09-22 16:37:54 -0400

    set -ex

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
 # workaround required to avoid libstdc++ errors while running "extended" hugo with SASS support
 wget http://security.ubuntu.com/ubuntu/pool/main/g/gcc-5/libstdc++6_5.4.0-6ubuntu1~16.04.10_amd64.deb
