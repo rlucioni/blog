58a2b1a2c5507bee8ee37ecc0cefc80308259a8e
Renzo Lucioni <renzo@lucioni.xyz>
2019-03-03 00:06:32 -0500

    use aws-cli snap

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 
-wget -O - https://raw.githubusercontent.com/creationix/nvm/v${NVM_VERSION}/install.sh | bash
-
-set +x
-source $HOME/.nvm/nvm.sh
-nvm install
-set -x
-
-npm install -g npm@${NPM_VERSION}
-
 node --version
 npm --version
 
 npm ci
-pip install -r requirements.txt
 
 hugo version
 
