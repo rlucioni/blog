d35e50915df96b7f69f2c12bf946d98f3c52987e
Renzo Lucioni <renzolucioni@gmail.com>
2020-08-19 19:58:45 -0400

    write to ~/bin

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 set -ex
 
 wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz" -O /tmp/hugo.tar.gz
-tar -xvzf /tmp/hugo.tar.gz -C /usr/local/bin
+tar -xvzf /tmp/hugo.tar.gz -C ~/bin
 
 which hugo
 hugo version
