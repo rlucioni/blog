4a37af4d44101f4ccd56ef6d7f3f855146922769
Renzo Lucioni <renzolucioni@gmail.com>
2020-08-19 20:43:46 -0400

    try aws cli v2

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-curl -o /tmp/hugo.tar.gz "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz"
+curl "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz" -o /tmp/hugo.tar.gz
 tar -xvzf /tmp/hugo.tar.gz -C ~/bin
 
 hugo version
 
-sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get -q update
-sudo apt-get -y install python3.8 python3-pip
-
-python3.8 --version
-pip3 --version
-
-pip3 install -r requirements.txt
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o /tmp/aws.zip
+unzip /tmp/aws.zip
+./tmp/aws/install --help
+./tmp/aws/install -i /tmp -b ~/bin
 
 aws --version
