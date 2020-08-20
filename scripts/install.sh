8a97f8121d63c8a57056656fce8092a2c6c346ea
Renzo Lucioni <renzolucioni@gmail.com>
2020-08-19 20:59:14 -0400

    unzip to working dir, pin aws version

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,15 +2,17 @@
 set -ex
 
 # https://gohugo.io/getting-started/installing/#install-hugo-from-tarball
+# https://github.com/gohugoio/hugo/releases
 curl -L "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz" -o /tmp/hugo.tar.gz
 tar -xvzf /tmp/hugo.tar.gz -C ~/bin
 
 hugo version
 
 # https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html#cliv2-linux-install
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o /tmp/aws.zip
-unzip /tmp/aws.zip -d /tmp
-./tmp/aws/install --help
-./tmp/aws/install -i /tmp -b ~/bin
+# https://github.com/aws/aws-cli/blob/v2/CHANGELOG.rst
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-${AWS_VERSION}.zip" -o /tmp/aws.zip
+unzip /tmp/aws.zip
+./aws/install --help
+./aws/install
 
 aws --version
