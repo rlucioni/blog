88a55a0590e2eae98027cb21057c93cb88fda7e0
Renzo Lucioni <renzolucioni@gmail.com>
2020-08-19 21:11:04 -0400

    npm ci

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
+npm ci
+
 # https://gohugo.io/getting-started/installing/#install-hugo-from-tarball
 # https://github.com/gohugoio/hugo/releases
 curl -L "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz" -o /tmp/hugo.tar.gz
